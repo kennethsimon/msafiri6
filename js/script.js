dddb75bf63f3cdef9b333c6aa0070723ce8bc5e4
kennethsimon <kenethsimon@gmail.com>
2019-04-08 14:20:16 +0300

    footer
    footer

--- js/script.js
+++ js/script.js
@@ -16,20 +16,20 @@ $(document).ready(function(){
     });
   
 });
-//document.addEventListener('contextmenu', event => event.preventDefault());
- // $('body').bind('cut copy paste', function (e) {
-      //  e.preventDefault();
-    //});
-    var sources = video.getElementsByTagName('source');
+document.addEventListener('contextmenu', event => event.preventDefault());
+  $('body').bind('cut copy paste', function (e) {
+        e.preventDefault();
+    });
+    //var sources = video.getElementsByTagName('source');
 if (window.Modernizr && window.Modernizr.on) {
     window.Modernizr.on('videoautoplay', function() {
-      for (var j = 0; j < sources.length; j++) {
+     for (var j = 0; j < sources.length; j++) {
         var source = sources[j];
         if (source.dataset.src != undefined) {
           var newSource = document.createElement("source");
-          newSource.setAttribute("src", source.dataset.src);
+         newSource.setAttribute("src", source.dataset.src);
           video.appendChild(newSource);
         }
-    }
+}
   });
 }
